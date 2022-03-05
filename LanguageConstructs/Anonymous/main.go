23ce2f2dc17aecaaea56c0077cce8598fa0ea30f
Hamza Khan <h.akhan@outlook.com>
2022-03-04 21:58:36 -0800

    added nested struct def example

--- LanguageConstructs/Anonymous/main.go
+++ LanguageConstructs/Anonymous/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "net"
+import "fmt"
 
+
+type Config struct {
+    Networking struct {
+	IP net.IP
+        Port int32
+    }
+}
+
+
+func main() {
+    c := Config {
+        Networking: struct {
+	    IP net.IP
+            Port int32
+        }{
+            IP: net.IP{8, 8, 8, 8},
+            Port: 42300,
+        },
+     }
+
+    fmt.Println(c.Networking.IP)
 }
