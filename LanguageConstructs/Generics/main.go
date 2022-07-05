cd5fc831abfd36b0af21e4df3f62612113aeb1b2
Hamza Khan <h.akhan@outlook.com>
2022-07-04 21:23:53 -0700

    added basic generic example

--- LanguageConstructs/Generics/main.go
+++ LanguageConstructs/Generics/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+type Added interface {
+	int32 | float32 | string
+}
+
+func Adder[T Added](a, b T) T {
+	return a + b
+}
+
+// Generic type
+type Collection[T Added] struct {
+	a, b T
+}
+
+func main() {
+	for _, pair := range []Collection[int32]{{1, 2}, {3, 4}} {
+		fmt.Println(Adder(pair.a, pair.b))
+	}
+
+	fmt.Println(Adder("hello ", "world"))
+
+	var c any
+	c = 123
+	fmt.Println(c.(int) + 23)
 }
